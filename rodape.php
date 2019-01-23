392493ffb94e62f9050ae168a1eb402ab135fb38
Darlan Souza Silva <darlan.sgf@gmail.com>
2019-01-23 16:46:29 -0200

    Rodape versão 1

--- rodape.php
+++ rodape.php
@@ -7,9 +7,24 @@
         include_once './valida_login.php';
         ?>
         <div class="rodape">
-            <pre>
-                Bancos de Dados II                                                                                                                                                                Alunos: Darlan Souza, Guilherme Rocha, Isack, Luiz Araújo
-            </pre>
-        </div>
+
+            
+            <div class="controdape">
+            <div class="icone">
+                <a href="https://www.facebook.com/"><img src="facebook.png"></a>
+                <a href="https://www.instagram.com/?hl=pt-br"><img src="insta.png"></a>
+            </div>
+            <div class="fundador">
+                <h4><center>Fundadores:<center></h4>
+                <h4><center>Darlan</center></h4>
+                <h4><center>Guilherme</center></h4>
+                <h4><center>Isac</center></h4>
+                <h4><center>Luiz</center></h4>
+            </div>
+            </div>
+            <div class="copy">
+                <p>@ copyright GereCurso</p>
+            </div>
+       </div>
     </body>
 </html>
