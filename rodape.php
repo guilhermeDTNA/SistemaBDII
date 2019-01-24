0339e76f66ce03a56d8099c58bb0d7833696acad
Luiz Araujo <luiz.araujodtna@gmail.com>
2019-01-24 09:30:45 -0200

    rodapé

--- rodape.php
+++ rodape.php
@@ -8,7 +8,7 @@
         ?>
         <div class="rodape">
             <pre>
-                Bancos de Dados II                                                                                                                                                                Alunos: Darlan Souza, Guilherme Rocha, Isack, Luiz Araújo
+                Bancos de Dados II                                                                                                                                                                 Alunos: Darlan Souza, Guilherme Rocha, Isack, Luiz Araújo
             </pre>
         </div>
     </body>
