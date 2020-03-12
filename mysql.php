80a23d37c7b1ecbf48a2574ee90ec655446d6140
Guilherme Rocha <45232536+guilhermeDTNA@users.noreply.github.com>
2020-03-12 11:41:49 -0300

    Alterando a senha

--- mysql.php
+++ mysql.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $pdo = new PDO("mysql:host=localhost;dbname=id8854008_sistema", "id8854008_guilhermedtna", "GNR*CAM");
+    $pdo = new PDO("mysql:host=localhost;dbname=id8854008_sistema", "id8854008_guilhermedtna", "senha");
     // Set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
