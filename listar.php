61f90c3139b7e051b3d104ec54cf1610561cb4c3
guilherme <gleite98@gmail.com>
2020-10-08 23:00:01 -0300

    Atualizando projeto, retirando link para portfólio e ignorando arquivos de conexão

--- listar.php
+++ listar.php
@@ -20,7 +20,6 @@
         $msg = addslashes($_GET['msg']);
         //include_once './topo.php';
         include_once './mysql.php';
-
         try {
 
             if ($msg == 'Professor-Aluno') {
